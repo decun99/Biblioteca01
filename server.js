fd3e0621daae1d6f2baf8a44b5d99793d8731e52
José Ignacio Alegria <jose.alegria@sansano.usm.cl>
2023-07-06 00:44:39 -0400

    agrego codigo con persistencia en mis_asignaturas

--- server.js
+++ server.js
@@ -6,14 +6,21 @@ app.use(cors());
 
 // Resto de la configuración del servidor
 
-let ruta = "./src/data/ramos.json";
+let rutaramos = "./src/data/ramos.json";
+let rutamisem = "./src/data/ramosMiSemestreMock.json";
 
 app.use(express.json());
 
 // Ruta para obtener el JSON
-app.get('/json', (req, res) => {
+app.get('/jsonramos', (req, res) => {
   // Aquí puedes leer el JSON desde el archivo y enviarlo como respuesta
-  const jsonData = require(ruta);
+  const jsonData = require(rutaramos);
+  res.json(jsonData);
+});
+
+app.get('/jsonmisem', (req, res) => {
+  // Aquí puedes leer el JSON desde el archivo y enviarlo como respuesta
+  const jsonData = require(rutamisem);
   res.json(jsonData);
 });
 
@@ -22,9 +29,9 @@ const fs = require('fs');
 const { json } = require('react-router-dom');
 
 // Ruta para editar el JSON
-app.post('/json', (req, res) => {
+app.post('/jsonramos', (req, res) => {
   const updatedData = req.body; // Datos enviados en el cuerpo de la solicitud
-  const jsonData = require(ruta); // Lee el JSON desde el archivo
+  const jsonData = require(rutaramos); // Lee el JSON desde el archivo
 
   //console.log("jsonData:", jsonData);
   //console.log("\nupdatedData:", updatedData);
@@ -36,19 +43,25 @@ app.post('/json', (req, res) => {
   const jsonContent = JSON.stringify(jsonData);
 
   // Sobrescribe el archivo con los nuevos datos
-  fs.writeFileSync(ruta, jsonContent, 'utf8');
+  fs.writeFileSync(rutaramos, jsonContent, 'utf8');
+});
 
-  /* // Actualiza el JSON en el servidor con los datos concatenados
-   fs.writeFile('./src/components/ramos.json', JSON.stringify(concatenatedData), 'utf8', (err) => {
-     if (err) {
-       console.error('Error al guardar los datos en el archivo:', err);
-       res.status(500).json({ success: false, error: 'Error al guardar los datos en el archivo' });
-     } else {
-       console.log('Datos guardados en el archivo correctamente');
-       res.json({ success: true });
-     }
-   });*/
-  //console.log(concatenatedData);
+// Ruta para editar el JSON
+app.post('/jsonmisem', (req, res) => {
+  const updatedData = req.body; // Datos enviados en el cuerpo de la solicitud
+  const jsonData = require(rutamisem); // Lee el JSON desde el archivo
+
+  //console.log("jsonData:", jsonData);
+  //console.log("\nupdatedData:", updatedData);
+
+  jsonData.push(updatedData);
+
+//  console.log("jsonData:", jsonData);
+
+  const jsonContent = JSON.stringify(jsonData);
+
+  // Sobrescribe el archivo con los nuevos datos
+  fs.writeFileSync(rutamisem, jsonContent, 'utf8');
 });
 
 // Inicia el servidor
